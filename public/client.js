474c6414e44fcf155b95e0a81f4f5702e9267634
Max Runesson <Max@maxRunesson.se>
2021-02-15 11:49:11 +0100

    en monter klar

--- public/client.js
+++ public/client.js
@@ -5,7 +5,7 @@ import Stats from '/jsm/libs/stats.module.js';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.z = 2;
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -13,36 +13,151 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1.5 ,1 ,.5);
+
+var mounter = function(){
+
+var textureLoader = new THREE.TextureLoader();
+var wallIMG = textureLoader.load( 'wall.jpg' );
+var kommunIMG = textureLoader.load( 'kommun.jpg' );
+var logoIMG = textureLoader.load( 'logo.png' );
+var infoIMG = textureLoader.load( 'info.png' );
+var galleriIMG = textureLoader.load( 'galleri1.png' );
+
+var wall = function(){
+const geometry = new THREE.BoxGeometry(5 ,3 ,.5);
+
+
 
 const cubeMaterials = [ 
-    new THREE.MeshBasicMaterial({color:0xff0000, transparent:true, opacity:0.8, side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({color:0x00ff00, transparent:true, opacity:0.8, side: THREE.DoubleSide}), 
-    new THREE.MeshBasicMaterial({color:0x0000ff, transparent:true, opacity:0.8, side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({color:0xffff00, transparent:true, opacity:0.8, side: THREE.DoubleSide}), 
-    new THREE.MeshBasicMaterial({color:0xff00ff, transparent:true, opacity:0.8, side: THREE.DoubleSide}), 
-    new THREE.MeshBasicMaterial({color:0x00ffff, transparent:true, opacity:0.8, side: THREE.DoubleSide}), 
+    new THREE.MeshBasicMaterial({color:0x808080, transparent:true, side: THREE.DoubleSide}),
+    new THREE.MeshBasicMaterial({color:0x808080, transparent:true, side: THREE.DoubleSide}), 
+    new THREE.MeshBasicMaterial({color:0x808080, transparent:true, side: THREE.DoubleSide}),
+    new THREE.MeshBasicMaterial({color:0x808080, transparent:true, side: THREE.DoubleSide}), 
+    new THREE.MeshBasicMaterial({ map: wallIMG }), 
+    new THREE.MeshBasicMaterial({color:0x808080, transparent:true, side: THREE.DoubleSide}), 
 ]; 
 
 const cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
 const cube = new THREE.Mesh(geometry, cubeMaterial);
-cube.position.x = 2;
 scene.add(cube);
+};
 
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    render();
-}, false);
+var logo = function(){
+    const geometry = new THREE.BoxGeometry(3.5 ,.5 ,.1);
+    
+    const cubeMaterials = [ 
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({ map: logoIMG }), 
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+    ]; 
+    
+    const cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
+    const cube = new THREE.Mesh(geometry, cubeMaterial);
+    
+    cube.position.x = .25;
+    cube.position.y = 1;
+    cube.position.z = .3;
+
+    scene.add(cube);
+};
+
+var kommun = function(){
+    const geometry = new THREE.BoxGeometry(.5, .5 ,.1);
+    
+    const cubeMaterials = [ 
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({ map: kommunIMG }), 
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}), 
+    ]; 
+    
+    const cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
+    const cube = new THREE.Mesh(geometry, cubeMaterial);
+    
+    cube.position.x = -1.75;
+    cube.position.y = 1;
+    cube.position.z = .3;
+
+    scene.add(cube);
+};
+
+var galleri = function(){
+    const geometry = new THREE.BoxGeometry(1.5 ,1 ,.1);
+    
+    const cubeMaterials = [ 
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}), 
+        new THREE.MeshBasicMaterial({ map: galleriIMG }), 
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+    ]; 
+    
+    const cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
+    const cube = new THREE.Mesh(geometry, cubeMaterial);
+    
+    cube.position.x = -1,5;
+    cube.position.y = 0;
+    cube.position.z = .3;
+
+    scene.add(cube);
+    
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        render();
+    }, false);
+};
+
+var info = function(){
+    const geometry = new THREE.BoxGeometry(1.5 ,1 ,.1);
+    
+    const cubeMaterials = [ 
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial({ map: infoIMG }), 
+        new THREE.MeshBasicMaterial({color:0xFFA500, side: THREE.DoubleSide}),
+    ]; 
+    
+    const cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
+    const cube = new THREE.Mesh(geometry, cubeMaterial);
+    
+    cube.position.x = 1,5;
+    cube.position.y = 0;
+    cube.position.z = .3;
+
+    scene.add(cube);
+    
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        render();
+    }, false);
+};
+wall();
+logo();
+kommun();
+galleri();
+info();
+};
+
+mounter();
+
 
 const stats = Stats();
 document.body.appendChild(stats.dom);
 
 var animate = function () {
     requestAnimationFrame(animate);
-    //cube.rotation.x += 0.01;
-    //cube.rotation.y += 0.01;
     controls.update();
     render();
     stats.update();
